180165135cd29494414ced6c2e18f11ad087e937
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-03-24 23:04:48 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
       <Navbar />
       <HeroSection />
       <CardSection />
-      {/* <About /> */}
-      {/* <Footer /> */}
+      <About />
+      <Footer />
     </>
   );
 }
