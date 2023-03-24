bf4649b7ca0b42351f5d6ac9016b6060c314e247
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-03-24 14:37:22 +0530

    ml-20 added

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ export const HeroSection = () => {
   return (
     <>
       <div className="container px-12 mx-auto flex justify-between items-center h-[30rem] ">
-        <div className="w-72 h-72 bg-red-800 rounded-full"></div>
+        <div className="w-72 h-72 ml-20 bg-purple-800 rounded-full"></div>
       </div>
     </>
   );
