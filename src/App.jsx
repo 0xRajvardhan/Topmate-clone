36db1f93169c18059797e238b8323e666d00023a
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-03-24 17:04:11 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { HeroSection } from "./components/hero";
 import { Navbar } from "./components/navbar";
+import { Card } from "./components/card";
+import { About } from "./components/about";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Card />
+      <About />
+      <Footer />
     </>
   );
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <>
+            <div>
+                <img src="@/assets/about.png" alt="about" />
+            </div>
+        </>
+    );
+};
