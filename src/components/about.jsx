c460c83219157d9ecd2f066c9c0835908a434656
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-03-24 21:45:35 +0530

    -

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,8 +3,20 @@ import React from "react";
 export const About = () => {
     return (
         <>
-            <div>
-                <img src="@/assets/about.png" alt="about" />
+            <div className="container px-12 mx-auto flex justify-between items-center h-[30rem]">
+                <div className="h-full pt-10">
+                    <div className="bg-red-400 w-60 h-60 ml-20 pt-0"/>
+                </div>
+                <div className="flex flex-col px-20 mb-12 text-xl font-semibold text-black mt-12">
+                    <p className="mt-5 ">I am Sindhu! I have scaled various consumer internet startups such as Betterhalf, PayTm Insider, FilterCopy, Dice Media, Gobble , Nutshell, Imumz. </p>
+                    <p className="mt-5">What have I done? 🔥 </p>
+                    <p className="mt-5">👉 Scaled Betterhalf app - Y combinator, Kunal Shah backed startup.</p>
+                    <p className="mt-5">👉 Scaled FilterCopy, Dice Media monthly views from 300 million to 500 million.</p>
+                    <p className="mt-5">👉 Promoted large-scale projects - Indian Super League, Pro Kabaddi League, Martin Garrix music festival, Arijit Singh Copncert, IPL, and 20 more events.</p>
+                    <p className="mt-5">👉 I am a mentor at Growth School and have taught more than 500 working professionals and college students.</p>
+                    <p className="mt-5">👉 Have mentored 25 first-time founders and leaders on startup scaling, app growth, social media strategy.</p>
+                    <p className="mt-5">👉 Linked In's Global Vice President named me as Top Creator.</p>
+                </div>
             </div>
         </>
     );
